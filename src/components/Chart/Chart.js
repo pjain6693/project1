fbfc25549eb90923eb8ce146066be58ea6cfc4a3
Pooja Jain <poojajain4395@gmail.com>
2022-07-29 14:25:24 +0530

    Added bar names and expense details

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -5,15 +5,18 @@ const Chart = (props) => {
   const itemsValue = props.items.map((dataPoint) => dataPoint.value);
   const totalMaximum = Math.max(...itemsValue);
   return (
-    <div className="chart">
-      {props.items.map((item) => (
-        <ChartBar
-          key={item.label}
-          value={item.value}
-          maxValue={totalMaximum}
-          lable={item.lable}
-        />
-      ))}
+    <div>
+      
+      <div className="chart">
+        {props.items.map((item) => (
+          <ChartBar
+            key={item.label}
+            value={item.value}
+            maxValue={totalMaximum}
+            label={item.label}
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -7,15 +7,25 @@ const ChartBar = (props) => {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
 
+  let color = "#4826b9";
+  if (props.value <= 1000) {
+    color = "#4826b9";
+  } else if (props.value > 1000 && props.value <= 3000) {
+    color = "yellow";
+  } else if (props.value > 3000) {
+    color = "red";
+  }
+
   return (
     <div className="chart-bar">
       <div className="chart-bar__inner">
         <div
           className="chart-bar__fill"
-          style={{ height: barFillHeight }}
+          style={{ height: barFillHeight, backgroundColor: color }}
         ></div>
       </div>
-      <div className="chart-bar__label"></div>
+      <div className="chart-bar__label">{props.label}</div>
+      <div className="chart-bar__label">{props.value}</div>
     </div>
   );
 };
